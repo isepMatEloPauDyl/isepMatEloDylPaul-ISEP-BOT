f0864f98ec36b1f5b97639fc63e521c73d5ec2dd
sacarabany <p.sacarabany@hotmail.fr>
2017-05-15 14:42:19 +0200

    mise à jour du fichier de config travis

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
+  token: 'MzEzNjQ5ODQ2MTM1NTU0MDUw.C_sstw.ojrqhBh_0_nIx7nazy0KWvYHURs',
   channel: 'channelid'
 }
