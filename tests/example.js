6ce850c31b1c883c367c4d3451709876b791bb80
Matthieuvuong <matthieu.vuong@gmail.com>
2017-06-11 22:28:40 +0200

    Test Unit test Travis deux

--- tests/example.js
+++ tests/example.js
@@ -1,7 +1,12 @@
 import test from 'ava'
 var client = require('node-rest-client-promise').Client()
+const config = require('../config.js')
+var translate = require('@google-cloud/translate')({
+  key: config.keyGoogle
+})
 
 test('Example test', t => {
+  console.log('jesuis dans la fct test open')
   return client.getPromise('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b05787eda8d8f7967925692ea52134d2')
     .catch((error) => {
       t.fail()
@@ -25,4 +30,20 @@ test('Example test', t => {
       console.log('TEST UNITAIRE pokeapi / fonction -> pokemon : Statut code : ' + res.response.statusCode)
       t.is(res.response.statusCode, 200)
     })
+})
+
+test('test googleTranslate', t => {
+  console.log('je suis dans la fct test translate')
+  translate.translate('hello', 'fr', function (err, translation) {
+    if (!err) {
+      console.log('TEST UNITAIRE  / fonction -> googleTranslate : Statut code : 200')
+      t.is(200)
+    }
+    if (err) {
+      t.fail()
+      console.log('TEST UNITAIRE googleTranslate / fonction -> googleTranslate  : Erreur')
+      console.log(err)
+      // Test
+    }
+  })
 })
