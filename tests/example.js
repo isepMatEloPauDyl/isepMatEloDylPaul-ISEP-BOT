d977ad107c30f23169b0cef082f0dcdbaeec57c2
Matthieuvuong <matthieu.vuong@gmail.com>
2017-06-11 22:19:00 +0200

    Test Unit test pour Travis

--- tests/example.js
+++ tests/example.js
@@ -1,9 +1,5 @@
 import test from 'ava'
 var client = require('node-rest-client-promise').Client()
-const config = require('../config.js')
-var translate = require('@google-cloud/translate')({
-  key: config.keyGoogle
-})
 
 test('Example test', t => {
   return client.getPromise('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b05787eda8d8f7967925692ea52134d2')
@@ -18,20 +14,15 @@ test('Example test', t => {
     })
 })
 
-test('test googleTranslate', t => {
-  return translate.translate('hello', 'fr', function (err, translation) {
-    if (!err) {
-      console.log('TEST UNITAIRE  / fonction -> googleTranslate : Statut code : 200')
-      t.is(200, 200)
-    } else {
-      console.log('TEST UNITAIRE  / fonction -> googleTranslate : Statut code : 200 : aucun résultat')
-      t.is(200, 200)
-    }
-    if (err) {
+test('Example test', t => {
+  return client.getPromise('https://pokeapi.co/api/v2/pokemon/charmander')
+    .catch((error) => {
       t.fail()
-      console.log('TEST UNITAIRE googleTranslate / fonction -> googleTranslate  : Erreur')
-      console.log(err)
-      // Test
-    }
-  })
+      // throw error
+      console.log(error)
+    })
+    .then((res) => {
+      console.log('TEST UNITAIRE pokeapi / fonction -> pokemon : Statut code : ' + res.response.statusCode)
+      t.is(res.response.statusCode, 200)
+    })
 })
